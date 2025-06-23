bde17033fcb628fc6c8b2c16b2d2983e0e18971f
Justin-Clyde-Eugenio-Villa <66011010@kmitl.ac.th>
2025-06-23 08:05:29 +0000

    Add py for P N

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number: "))
+
+if n > 0:
+    print("This number is positive")
+elif n < 0:
+    print("This number is negative")
+else:
+    print("This number is both postive and negative")
