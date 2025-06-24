3e2ae7198196632eb36bf476cb0464c3359b4104
Justin-Clyde-Eugenio-Villa <66011010@kmitl.ac.th>
2025-06-24 05:16:00 +0000

    Added new array

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+org_arr = [2, 3, 8, 7, 42, -10, 2]
+new_arr = []
+
+for i in org_arr:
+    new_arr.append(i + 2)
+
+print(f"Original Array {org_arr}")
+print(f"New array: {new_arr}")
